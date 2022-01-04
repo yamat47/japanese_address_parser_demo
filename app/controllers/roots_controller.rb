80659aa1a7b14738c6603a0d33270e27d9d63177
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-01-04 18:38:35 +0900

    Add search function by query parameter.

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,4 +1,12 @@
 class RootsController < ApplicationController
   def show
+    @search = search_params[:search] || ''
+    @address = JapaneseAddressParser.call(@search)
+  end
+
+  private
+
+  def search_params
+    params.permit(:search)
   end
 end
