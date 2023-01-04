05bdec7ce5ec3683b6dc56109f558293edce94d4
yamat47 <yamat47.thirddown@gmail.com>
2023-01-04 23:48:33 +0900

    Add non-parsable address's example.

--- spec/system/roots/show_spec.rb
+++ spec/system/roots/show_spec.rb
@@ -10,8 +10,22 @@ RSpec.describe 'Roots#show page' do
     click_button '解析'
 
     expect(page).to have_text '解析結果'
-    expect(page).to have_text 'TOKYO TO'
-    expect(page).to have_text 'MINATO KU'
-    expect(page).to have_text 'SHIBAKOEN 4'
+    expect(page.find_by_id('prefecture')).to have_text 'TOKYO TO'
+    expect(page.find_by_id('city')).to have_text 'MINATO KU'
+    expect(page.find_by_id('town')).to have_text 'SHIBAKOEN 4'
+  end
+
+  specify 'Lumen Field\'s address is not parsable.' do
+    visit '/'
+
+    expect(page).to have_css('h1', text: 'JapaneseAddressParser')
+
+    fill_in 'search',	with: '800 Occidental Ave. S. Seattle, WA 98134'
+    click_button '解析'
+
+    expect(page).to have_text '解析結果'
+    expect(page.find_by_id('prefecture')).to have_text '見つかりませんでした。'
+    expect(page.find_by_id('city')).to have_text '見つかりませんでした。'
+    expect(page.find_by_id('town')).to have_text '見つかりませんでした。'
   end
 end
